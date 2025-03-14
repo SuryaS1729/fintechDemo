7a6ef19a13135f44440cd791de8a3cd15bf06393
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-14 17:00:35 +0530

    added prisma

--- apps/fintech-user-app/app/page.tsx
+++ apps/fintech-user-app/app/page.tsx
@@ -1,4 +1,6 @@
+import { PrismaClient } from "@repo/db/client";
 
+const client = new PrismaClient();
 export default function Home() {
   return (
     <div className="text-3xl font-bold underline">hey there</div>
